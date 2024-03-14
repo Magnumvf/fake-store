b9a5d376019979510b1303775deff8e17edf3544
gleidsonteixeira <gleidsondeveloper@gmail.com>
2024-03-14 11:07:43 -0300

    updates

--- js/index.js
+++ js/index.js
@@ -5,21 +5,19 @@ function getCategories(){
         for(let i = 0; i < response.length; i++){
             categorias.innerHTML += `<option>${response[i]}</option>`;
         }
-        // response.map(r => {
-        //     categorias.innerHTML += `<option>${r}</option>`;
-        // })
     })
 } getCategories();
 
 async function getProducts(){
-    const request = await fetch('https://fakestoreapi.com/products');
-    const response = await request.json();
-    if(response.length > 0){
+    try {
+        const request = await fetch('https://fakestoreapi.com/products');
+        const response = await request.json();
+        produtos.innerHTML = '';
         response.map(produto => {
             produtos.innerHTML += `<li>${produto.title}</li>`;
         })
         return;
+    } catch (error) {
+        alert("produtos não encontrados :/");
     }
-
-    alert("produtos não encontrados :/");
 } getProducts();
